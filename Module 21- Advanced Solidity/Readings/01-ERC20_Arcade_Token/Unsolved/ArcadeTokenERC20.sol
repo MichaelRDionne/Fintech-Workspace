c47afc9fcd597cbcdb091bea20e2aa194cf8270d
Michael <michael@digitalgoldrush.co>
2022-10-30 21:06:44 -0700

    update

--- Module 21- Advanced Solidity/Readings/01-ERC20_Arcade_Token/Unsolved/ArcadeTokenERC20.sol
+++ Module 21- Advanced Solidity/Readings/01-ERC20_Arcade_Token/Unsolved/ArcadeTokenERC20.sol
@@ -55,3 +55,23 @@ function mint(address recipient, uint amount) public onlyOwner {
 
 pragma solidity ^0.5.17;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract ArcadeToken is ERC20, ERC20Detailed {
+    address payable owner;
+
+    modifier onlyowner {
+        require(msg.sender == owner, " You do not have permission to mint these tokens!");
+        _;
+    }
+
+    constructor(uint initial_supply) ERC20Detailed("ArcadeToken", "ARCD", 18) public {
+    owner = msg.sender;
+    _mint(owner, initial_supply);
+    }
+
+    function mint(address recipient, uint amount) public onlyowner{
+        _mint(recipient, amount);
+    }
+}
