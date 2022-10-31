c770e55114661b94f1bcf884ded960d11231640d
Michael <michael@digitalgoldrush.co>
2022-10-30 20:47:52 -0700

    update

--- Module 21- Advanced Solidity/Readings/01-Arcade_Token/Unsolved/ArcadeToken.sol
+++ Module 21- Advanced Solidity/Readings/01-Arcade_Token/Unsolved/ArcadeToken.sol
@@ -50,3 +50,30 @@ Instructions:
 */
 
 // Construct your ArcadeToken contract below:
+
+pragma solidity ^0.5.17;
+
+contract ArcadeToken {
+   address payable owner = msg.sender;
+   string public symbol = "ARCD";
+   uint public exchange_rate = 100;
+
+   mapping (address => uint) balances;
+
+   function balance () public view returns (uint) {
+      return balances[msg_sender];
+   }
+   function transfer (address recipient, uint value) public {
+      balances[msg.sender] -=value;
+      balances[recipient] +=value;
+   } 
+   function purchase () public payable {
+      uint amount = msg_value * exchange_rate;
+      balances[msg_sender] += amount;
+      owner.transfer(msg.value);
+   }
+   function mint (address recipient, uint value) {
+      require(msg_sender == owner, " You do not have permission to mint.");
+      balances[recipient] +=value;
+   }
+}
