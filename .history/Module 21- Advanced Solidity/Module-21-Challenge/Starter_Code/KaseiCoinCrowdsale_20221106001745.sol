8744c9bbd29fc247db619850d1003466fc614158
Michael <michael@digitalgoldrush.co>
2022-11-06 00:18:03 -0700

    update code

---
+++ .history/Module 21- Advanced Solidity/Module-21-Challenge/Starter_Code/KaseiCoinCrowdsale_20221106001745.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.5.0;
+
+import "./KaseiCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
+    
+    constructor(
+        uint256 rate, // rate in TKNbits
+        address payable wallet, // sale beneficiary
+        KaseiCoin token, // the KaseiCoin itself that the KaseiCoinCrowdsale will work with
+        uint goal, // the crowdsale goal
+        uint open, // the crowdsale opening time
+        uint close // the crowdsale closing time
+    ) public
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(100)
+        TimedCrowdsale(now, 4 minutes)
+        RefundableCrowdsale(50)
+    {
+        // constructor can stay empty
+    }
+}
+
+contract KaseiCoinCrowdsaleDeployer {
+    // Create an `address public` variable called `kasei_token_address`.
+    address public kasei_token_address;
+    // Create an `address public` variable called `kasei_crowdsale_address`.
+    address public kasei_crowdsale_address;
+
+    constructor(
+       string memory name,
+       string memory symbol,
+       address payable wallet // receiving wallet of all ETH from the sale
+    ) public {
+       
+        // Create a new instance of the KaseiCoin contract.
+        KaseiCoin token = new KaseiCoin(name, symbol, 0);
+        kasei_token_address = address(token);
+
+        // Create a new instance of the `KaseiCoinCrowdsale` contract
+        KaseiCoinCrowdsale kasei_crowdsale = 
+            new KaseiCoinCrowdsale (1, wallet, token, 100, now, now + 4 minutes);
+            
+        // Assign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
+        kasei_crowdsale_address = address(kasei_crowdsale);
+
+        // Set the `KaseiCoinCrowdsale` contract as a minter
+        token.addMinter(kasei_crowdsale_address);
+        
+        // Have the `KaseiCoinCrowdsaleDeployer` renounce its minter role.
+        token.renounceMinter();
+    }
+}
