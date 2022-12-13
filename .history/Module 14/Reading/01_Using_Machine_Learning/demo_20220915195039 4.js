1e3591fefef6274cbf2d977350e727a759a76994
Michael <michael@digitalgoldrush.co>
2022-12-12 20:39:52 -0800

    update

---
+++ .history/Module 14/Reading/01_Using_Machine_Learning/demo_20220915195039 4.js
@@ -0,0 +1,11 @@
+extension.enableBlackBoxAutoComplete
+// Language: javascript
+"// plot the SVM classification report vs the Logistic Regression classification report."
+// Language: javascript
+// Path: Module 14/Reading/01_Using_Machine_Learning/demo.js
+extension.enableBlackBoxAutoComplete
+// Language: javascript
+"// plot the SVM classification report vs the Logistic Regression classification report."
+// Language: javascript
+// Path: Module 14/Reading/01_Using_Machine_Learning/demo.js
+extension.enableBlackBoxAutoComplete
