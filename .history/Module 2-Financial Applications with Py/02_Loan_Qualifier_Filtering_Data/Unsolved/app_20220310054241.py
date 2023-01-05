f320d4fd7841f31f6347980ce03ad925c14eef95
Michael <michael@digitalgoldrush.co>
2023-01-04 19:03:38 -0800

    update

--- .history/Module 2-Financial Applications with Py/02_Loan_Qualifier_Filtering_Data/Unsolved/app_20220310054241.py
+++ .history/Module 2-Financial Applications with Py/02_Loan_Qualifier_Filtering_Data/Unsolved/app_20220310054241.py
@@ -45,13 +45,8 @@ def calculate_loan_to_value_ratio(loan_amount, home_value):
 # I want to filter the bank list by checking the customer's desired loan against the bank's maximum loan size
 # so that we can know which banks offer the loan amount requested by the customer
 def filter_max_loan_size(loan_amount, bank_list):
-    loan_size_approval_list =[]
 
-    for bank in bank_list:
-        if loan_amount <= int(bank[1]):
-            loan_size_approval_list.append(bank)
 
-    return loan_size_approval_list
 
 # @TODO Define a function that implements the following user story:
 # As a lender,
